98243ba8f328d04e5b740061b1abd2e68927a6e4
kthatake7 <kota.hatakeyama@rakus-partners.co.jp>
2020-04-13 16:49:37 +0900

    テスト用のBookAPIController登録

---
+++ src/main/java/com/honcari/controller/BookApiTestController.java
@@ -0,0 +1,13 @@
+package com.honcari.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookApiTestController {
+
+	@RequestMapping("")
+	public String test() {
+		return "test";
+	}
+}
