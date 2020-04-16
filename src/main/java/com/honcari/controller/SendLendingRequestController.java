869045dd9564afabab831c0d09c6059a803124e4
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-04-16 21:16:20 +0900

    貸出リクエストにエラーチェック追加（エラー表示方法検討中）

--- src/main/java/com/honcari/controller/SendLendingRequestController.java
+++ src/main/java/com/honcari/controller/SendLendingRequestController.java
@@ -4,6 +4,9 @@ import java.sql.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,32 +19,41 @@ import com.honcari.service.BookService;
  * @author shumpei
  *
  */
-@RequestMapping("")
 @Controller
+@RequestMapping("")
 public class SendLendingRequestController {
-	
+
 	@Autowired
 	private BookService bookService;
-	
+
 	@ModelAttribute
 	public LendingRequestForm setUpForm() {
 		return new LendingRequestForm();
 	}
-	
+
 	/**
 	 * 貸出リクエストを送る.
 	 * 
 	 * @param form フォーム
 	 * @return 貸出承認待ち一覧画面
 	 */
-	@RequestMapping("/send-lending-request")
-	public String sendLendingRequest(LendingRequestForm form) {
-		Integer borrowUserId = 1; //SpringSecurity実装後LoginUserへ置き換え
+	@RequestMapping("/send_lending_request")
+	public String sendLendingRequest(Model model, @Validated LendingRequestForm form, BindingResult result) {
+		Integer borrowUserId = 1; // TODO SpringSecurity実装後LoginUserへ置き換え
 		Integer bookId = form.getBookId();
 		Integer lendUserId = form.getLenderUserId();
+
+		if (borrowUserId == lendUserId) {
+			result.rejectValue("deadline", "500", "不正なリクエストが行われました");
+		}
+
+		if (result.hasErrors()) {
+			return "forward:/show_book_detail?bookId=" + bookId;
+		}
+
 		Date deadline = Date.valueOf(form.getDeadline());
 		bookService.runLendingBookRequest(bookId, lendUserId, borrowUserId, deadline);
-		//TODO 貸し手にメール送信
+		// TODO 貸し手にメール送信
 		return "redirect:/";
 	}
 
