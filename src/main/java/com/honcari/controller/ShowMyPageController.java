fa21e049f614b2d87ea2d48e510187537067c5d6
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-04-16 11:42:15 +0900

    参照するユーザーIDを変更しました。

--- src/main/java/com/honcari/controller/ShowMyPageController.java
+++ src/main/java/com/honcari/controller/ShowMyPageController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.honcari.domain.User;
 import com.honcari.service.ShowMyPageService;
 
+/**
+ * マイページを表示するコントローラー.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
 @Controller
 @RequestMapping("")
 public class ShowMyPageController {
@@ -15,10 +21,17 @@ public class ShowMyPageController {
 	@Autowired
 	private ShowMyPageService showMyPageService;
 	
-	@RequestMapping("/showMyPage")
-	public String showMyPage(Integer userId,Model model) {
-		userId = 1;
-		User user = showMyPageService.showUserInfo(userId);
+	/**
+	 * マイページに遷移するメソッド.
+	 * 
+	 * @param userId ユーザーID
+	 * @param model リクエストスコープ
+	 * @return マイページ画面
+	 */
+	@RequestMapping("/show_my_page")
+	public String showMyPage(Integer userId, Model model) {
+		userId = 2;
+		User user = showMyPageService.showUser(userId);
 		model.addAttribute("user",user);
 		return "mypage";
 	}
