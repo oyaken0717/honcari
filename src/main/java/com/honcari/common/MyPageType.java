2f3b99ca56c237ea76a67793ed3199f39d5db0d1
kthatake7 <kota.hatakeyama@rakus-partners.co.jp>
2020-04-21 10:31:49 +0900

    MyPage表示のためのEnumにカテゴリ別の変数を用意

--- src/main/java/com/honcari/common/MyPageType.java
+++ src/main/java/com/honcari/common/MyPageType.java
@@ -17,6 +17,24 @@ public enum MyPageType {
 	LENTAL_HISTORY(4),
 	/** カテゴリー一覧 */
 	EDIT_PROFILE(5),
+	/**	文学・小説 */
+	MY_BOOK_FICTION(6),
+	/** 社会・ビジネス	*/
+	MY_BOOK_BUSINESS(7),
+	/**	実用・教育 */
+	MY_BOOK_EDUCATION(8),
+	/** 旅行・地図 */
+	MY_BOOK_MAP(9),
+	/**	趣味 */
+	MY_BOOK_HOBBY(10),
+	/**	アート・教養・エンタメ */
+	MY_BOOK_ENTERTAINMENT(11),
+	/** 事典・図鑑・語学・辞書 */
+	MY_BOOK_LANGUAGE(12),
+	/** こども */
+	MY_BOOK_CHILDREN(13),
+	/** その他 */
+	MY_BOOK_OTHER(14)
 	;
 	
 	/** key値 */
