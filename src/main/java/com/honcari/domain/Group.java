edc67c4be96a61caf9e36a7456cbf79f040a0b46
yamadadai <dai.ymd6@gmail.com>
2020-04-24 15:08:33 +0900

    GroupとGroupRelationドメインを変更

--- src/main/java/com/honcari/domain/Group.java
+++ src/main/java/com/honcari/domain/Group.java
@@ -3,44 +3,75 @@ package com.honcari.domain;
 import java.util.List;
 
 public class Group {
-	
+
 	private Integer id;
 	private String name;
 	private String description;
+	private Integer ownerUserId;
+	private Boolean isPrivate;
+	private Boolean deleted;
 	private List<User> userList;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public Integer getOwnerUserId() {
+		return ownerUserId;
+	}
+
+	public void setOwnerUserId(Integer ownerUserId) {
+		this.ownerUserId = ownerUserId;
+	}
+
+	public Boolean getIsPrivate() {
+		return isPrivate;
+	}
+
+	public void setIsPrivate(Boolean isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
 	public List<User> getUserList() {
 		return userList;
 	}
+
 	public void setUserList(List<User> userList) {
 		this.userList = userList;
 	}
+
 	@Override
 	public String toString() {
-		return "Group [id=" + id + ", name=" + name + ", description=" + description + ", userList=" + userList + "]";
+		return "Group [id=" + id + ", name=" + name + ", description=" + description + ", ownerUserId=" + ownerUserId
+				+ ", isPrivate=" + isPrivate + ", deleted=" + deleted + ", userList=" + userList + "]";
 	}
-	
-	
-	
-	
-	
-	
 
 }
