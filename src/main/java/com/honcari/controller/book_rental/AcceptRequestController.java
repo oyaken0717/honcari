3cfb327f1a598a7f87704c1cc932279124c9b9eb
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-04-25 20:15:49 +0900

    貸出申請承認機能の実装（修正）

---
+++ src/main/java/com/honcari/controller/book_rental/AcceptRequestController.java
@@ -0,0 +1,36 @@
+package com.honcari.controller.book_rental;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.honcari.service.book_rental.AcceptRentalRequestService;
+
+/**
+ * 貸出申請を承認する.
+ * 
+ * @author shumpei
+ *
+ */
+@RequestMapping("/book_rental")
+@Controller
+public class AcceptRequestController {
+	
+	@Autowired
+	private AcceptRentalRequestService acceptRentalRequestService;
+	
+
+	/**
+	 * 貸出申請を承認する.
+	 * 
+	 * @param bookRentalId 貸出申請ID
+	 * @return 貸出情報一覧画面
+	 */
+	@RequestMapping("/accept")
+	public String acceptRequest(Integer bookRentalId) {
+		acceptRentalRequestService.acceptRentalRequest(bookRentalId);
+		// TODO 借り手にメール送信
+		return "redirect:/book_rental/rental_list";
+	}
+
+}
