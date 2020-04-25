71587eaedb821176637227f75bfcceb3848aa163
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-04-25 20:12:15 +0900

    貸出申請のキャンセル機能実装（修正）

---
+++ src/main/java/com/honcari/controller/book_rental/CancelRequestController.java
@@ -0,0 +1,35 @@
+package com.honcari.controller.book_rental;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.honcari.service.book_rental.CancelRentalRequestService;
+
+/**
+ * 貸出申請をキャンセルする.
+ * 
+ * @author shumpei
+ *
+ */
+@RequestMapping("/book_rental")
+@Controller
+public class CancelRequestController {
+
+	@Autowired
+	private CancelRentalRequestService cancelRentalRequestService;
+
+	/**
+	 * 貸出申請をキャンセルする.
+	 * 
+	 * @param bookRentalId 貸出情報ID
+	 * @return 貸出情報一覧画面
+	 */
+	@RequestMapping("/cancel")
+	public String cancelRequest(Integer bookRentalId) {
+		cancelRentalRequestService.cancelRentalRequest(bookRentalId);
+		// TODO 貸し手にメール送信
+		return "redirect:/book_rental/rental_list";
+	}
+
+}
