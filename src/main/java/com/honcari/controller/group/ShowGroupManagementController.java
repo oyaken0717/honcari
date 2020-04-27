da4980a4c8f2d67ba1e65ab16e4bcbf5f6fe5c6a
yamasekishoya <shoya.4617@gmail.com>
2020-04-27 23:41:05 +0900

    微修正

---
+++ src/main/java/com/honcari/controller/group/ShowGroupManagementController.java
@@ -0,0 +1,35 @@
+package com.honcari.controller.group;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.honcari.domain.Group;
+import com.honcari.domain.LoginUser;
+import com.honcari.domain.User;
+import com.honcari.service.group.ShowGroupManagementService;
+
+@Controller
+@RequestMapping(value="group")
+public class ShowGroupManagementController {
+	
+	@Autowired
+	private ShowGroupManagementService showGroupListService;
+	
+	@RequestMapping(value="/to_group_management")
+	public String showBelongGroup(@AuthenticationPrincipal LoginUser loginUser,Model model) {
+		User user = showGroupListService.showGroupListByBelongUserId(loginUser.getUser().getUserId());
+		List<Group> ownGroupList = showGroupListService.showGroupListByOwnerUserId(loginUser.getUser().getUserId());
+
+		model.addAttribute("user",user);
+		model.addAttribute("ownGroupList",ownGroupList);
+		user.getGroupList().stream().forEach(g -> System.out.println(g.getName()));
+		return "group/group_management";
+	}
+
+}
+
