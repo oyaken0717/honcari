515c2f88cb77455898b578242711e7d9120f7c84
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-04-28 10:18:58 +0900

    本詳細ページのUI変更（ボタン無効化・貸出状況スタイル追加）

--- src/main/resources/static/js/book_detail.js
+++ src/main/resources/static/js/book_detail.js
@@ -1,5 +1,18 @@
 $(function() {
+	// 入力値チェックでエラーが出た場合はモーダルを表示
 	if (document.getElementById("error-message") !== null) {
 		$("#rental-request-form").modal();
 	}
+
+	// 貸出状況に応じてボタンを無効化
+	let bookStatus = document.getElementById("book-status")
+	let bookStatusNumber = bookStatus.getAttribute("value");
+	const button = document.getElementById("rental-request-button")
+	if(bookStatusNumber == 0 || bookStatusNumber == 2 || bookStatusNumber == 3){
+		bookStatus.style.color = "rgb(255, 0, 0)"
+		button.disabled = true;
+	}else{
+		bookStatus.style.color = "rgb(0, 0, 255)"
+	}
+
 });
