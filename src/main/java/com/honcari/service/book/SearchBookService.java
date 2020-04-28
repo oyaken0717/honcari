9139a5911e497f036503b7ab7448410cfb782d58
yamadadai <dai.ymd6@gmail.com>
2020-04-28 12:05:06 +0900

    トップページの本のあいまい検索機能の実装

--- src/main/java/com/honcari/service/book/SearchBookService.java
+++ src/main/java/com/honcari/service/book/SearchBookService.java
@@ -1,7 +1,13 @@
 package com.honcari.service.book;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.honcari.domain.Category;
+import com.honcari.repository.CategoryRepository;
 
 /**
  * ユーザー情報に関するサービスクラス.
@@ -13,10 +19,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class SearchBookService {
 
-//	@Autowired
-//	private UserRepository userRepository;
-//	
-//	public List<User> findByGroupAndTitle(Integer groupId, String title) {
-//		return userRepository.findByGroupAndTitle(groupId, title);
-//	}
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	/**
+	 * ユーザーIDとタイトル名からカテゴリーごとの本情報を取得する.
+	 * 
+	 * @param userId ユーザーID
+	 * @param title タイトル名
+	 * @return カテゴリ一覧
+	 */
+	public List<Category> findByUserIdAndTitle(Integer userId, String title) {
+		return categoryRepository.findByUserIdAndTitle(userId, title);
+	}
 }
