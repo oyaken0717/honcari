964253615d0430073e421a26b133f7c2991259e8
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-04-28 18:04:55 +0900

    貸出申請時の楽観ロックを実装

--- src/main/java/com/honcari/controller/book_rental/SendRequestController.java
+++ src/main/java/com/honcari/controller/book_rental/SendRequestController.java
@@ -55,6 +55,9 @@ public class SendRequestController {
 		Integer ownedBookInfoId = form.getOwnedBookInfoId();
 		Integer bookStatus = form.getBookStatus();
 		Integer ownerId = form.getOwnerId();
+		Integer version = form.getVersion();
+		
+		//貸出状況のエラーチェック
 		if (bookStatus != 1 || ownerId == borrowUserId) {
 			result.rejectValue("deadline", "500", "不正なリクエストが行われました");
 		}
@@ -63,27 +66,33 @@ public class SendRequestController {
 			return showBookDetailController.showBookDetail(model, ownedBookInfoId);
 		}
 
+		//貸出期限のエラーチェック　
 		String inputDeadline = form.getDeadline();
 		Date deadline = Date.valueOf(inputDeadline);
 		LocalDate requestDate = LocalDate.now();
 		LocalDate deadlineDate = LocalDate.parse(inputDeadline);
 		LocalDate maxRequestDate = requestDate.plusMonths(2);
-		
 		if (deadlineDate.isBefore(requestDate)) {
 			result.rejectValue("deadline", "500", "貸出期限は今日以降の日付を入力してください");
 		}
-
 		if (deadlineDate.isAfter(maxRequestDate)) {
 			result.rejectValue("deadline", "500", "貸出期限は本日から2か月以内の日付を入力してください");
 		}
-
 		if (result.hasErrors()) {
 			return showBookDetailController.showBookDetail(model, ownedBookInfoId);
 		}
-
-		sendRentalRequestService.sendRentalRequest(ownedBookInfoId, borrowUserId, borrowUserName, deadline);
-		// TODO 貸し手にメール送信
+		
+		//貸出申請を処理する
+		try {
+			sendRentalRequestService.sendRentalRequest(ownedBookInfoId, borrowUserId, borrowUserName, deadline, version);			
+			// TODO 貸し手にメール送信
+		}catch (Exception ex) {
+			ex.printStackTrace();
+			result.rejectValue("deadline", "500", "貸出申請に失敗しました");
+			return showBookDetailController.showBookDetail(model, ownedBookInfoId);
+		}
 		return "redirect:/book_rental/show_list";
 	}
+	
 
 }
