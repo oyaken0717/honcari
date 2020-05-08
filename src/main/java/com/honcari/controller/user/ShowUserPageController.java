fa1aadb4c3f41abd5fc5a07bb969008ffaa85db4
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-05-08 15:34:55 +0900

    各ユーザーのプロフィール画面を表示するコントローラーを作成しました。

---
+++ src/main/java/com/honcari/controller/user/ShowUserPageController.java
@@ -0,0 +1,38 @@
+package com.honcari.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.honcari.domain.User;
+import com.honcari.service.user.SearchUserByUserIdService;
+
+/**
+ * ユーザーページを表示するコントローラー.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
+@Controller
+@RequestMapping("/user")
+public class ShowUserPageController {
+	
+	@Autowired
+	private SearchUserByUserIdService searchUserByUserIdService;
+
+	/**
+	 * ユーザーページに遷移するメソッド.
+	 * 
+	 * @param userId ユーザーID
+	 * @param model  リクエストスコープ
+	 * @return マイページ画面
+	 */
+	@RequestMapping("/show_user_page")
+	public String showUserPage(Integer userId, Model model) {
+		User user = searchUserByUserIdService.showUser(userId);
+		model.addAttribute("user", user);
+		return "user/user_page";
+	}
+
+}
