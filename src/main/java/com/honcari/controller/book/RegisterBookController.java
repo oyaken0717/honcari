ad9c23f3ca68bca9d6ee260ca4bf5017b1c41192
kthatake7 <kota.hatakeyama@rakus-partners.co.jp>
2020-05-11 16:18:58 +0900

    コード整理(コメント等の追加、仕様変更なし)

--- src/main/java/com/honcari/controller/book/RegisterBookController.java
+++ src/main/java/com/honcari/controller/book/RegisterBookController.java
@@ -27,7 +27,6 @@ import com.honcari.service.book.GoogleBookApiService;
 @RequestMapping("/book")
 public class RegisterBookController {
 
-
 	@Autowired
 	private FindAllCategoryService getAllCategoryService;
 	
@@ -53,69 +52,27 @@ public class RegisterBookController {
 	
 	@RequestMapping("/search_books")
 	public String getGoogleBooks(@Validated SearchGoogleBookForm searchGoogleBookForm, BindingResult result, Model model) {
-		Integer pageNumber = searchGoogleBookForm.getPageNumber();
+		
+		//検索ワードが入力されていない場合エラーを発生させる
 		if(result.hasErrors()) {
 			return showRegisterBook(model);
 		}
-		if(searchGoogleBookForm.getPageNumber() == null) {
+		
+		//現在のページを取得する
+		Integer pageNumber = searchGoogleBookForm.getPageNumber();
+		if(pageNumber == null) {
 			pageNumber = 1;
 		}
+		
+		//GoogleBooksAPIより書籍情報を取得し、リクエストスコープに格納する(検索結果の0件目から(pageNumber - 1)*17件を飛ばして15件取得する)
+		//TODO 計算おかしい…？
 		GoogleBooks googleBooks = googleBookApiService.getBook(searchGoogleBookForm.getSearchWord(), pageNumber - 1);
 		List<VolumeInfo> volumeInfoList = googleBooks.getItems();
 		model.addAttribute("volumeInfoList", volumeInfoList);
 		List<Category> categoryList = getAllCategoryService.findAll();
 		model.addAttribute("categoryList", categoryList);
-		model.addAttribute("nowPageNumber", pageNumber);
 		model.addAttribute("searchWord", searchGoogleBookForm.getSearchWord());
+		model.addAttribute("nowPageNumber", pageNumber);
 		return "book/register_book";
 	}
-	
-//TODO 非同期処理にて実装したため、コメントアウト　あとで消します
-//	/**
-//	 * 書籍登録画面で送られたリクエストパラメータ情報をowned_book_infoテーブルに登録する
-//	 * 
-//	 * @param registerBookForm 書籍登録画面で送られたリクエストパラメータ
-//	 * @param loginUser ログインしているユーザ情報
-//	 * @param redirectAttributes リダイレクト先へリクエストスコープを格納する
-//	 * @return 書籍登録画面
-//	 */
-//	@RequestMapping(value="/register", method=RequestMethod.POST)
-//	public String registerBook(RegisterBookForm registerBookForm, @AuthenticationPrincipal LoginUser loginUser, RedirectAttributes redirectAttributes) {
-//		Integer bookId = null;
-//		Book book = new Book();
-//		OwnedBookInfo ownedBookInfo = new OwnedBookInfo();
-//		
-//		//booksテーブルに既に該当の書籍が登録されているかISBNコードを用いて検索する
-//		List<Book> bookList = findByIsbnIdService.getByIsbnId(registerBookForm.getIsbnId());
-//		
-//		//検索出来ない場合、書籍情報をbooksテーブルに挿入しbook_idを戻り値として取得しbookIdに代入する
-//		//検索出来た場合、bookList0番目のbook_idを取得しbookIdに代入する
-//		if(bookList.isEmpty()) {
-//			Integer pageCount = null;
-//			if(registerBookForm.getPageCount().equals("undefined")) {
-//				pageCount = 0; //TODO テーブル側でdefault値入れてnullでも対応出来るようにする？
-//			}else {
-//				pageCount = Integer.parseInt(registerBookForm.getPageCount());
-//			}
-//			book.setIsbnId(registerBookForm.getIsbnId());
-//			book.setTitle(registerBookForm.getTitle());
-//			book.setAuthor(registerBookForm.getAuthor());
-//			book.setPublishedDate(registerBookForm.getPublishedDate());
-//			book.setDescription(registerBookForm.getDescription());
-//			book.setPageCount(pageCount);
-//			book.setThumbnailPath(registerBookForm.getThumbnailPath());
-//			Book registeredBook = registerBookService.registerBook(book);
-//			bookId = registeredBook.getBookId();
-//		}else {
-//			bookId = bookList.get(0).getBookId();
-//		}
-//		ownedBookInfo.setUserId(loginUser.getUser().getUserId());
-//		ownedBookInfo.setBookId(bookId);
-//		ownedBookInfo.setCategoryId(Integer.parseInt(registerBookForm.getCategoryId()));
-//		ownedBookInfo.setBookStatus(1);
-//		ownedBookInfo.setComment(registerBookForm.getComment());
-//		registerOwnedBookInfoService.registerOwnedBookInfo(ownedBookInfo);
-//		redirectAttributes.addFlashAttribute("check", "check");
-//		return "redirect:/book/show_register";
-//	}
 }
