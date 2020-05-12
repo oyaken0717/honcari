3aaf4ada77cf00a49efa21a923caa9ba38bdda23
yamasekishoya <shoya.4617@gmail.com>
2020-05-12 18:45:38 +0900

    グループ周辺の見出し画像変更

--- src/main/java/com/honcari/controller/ReturnPageController.java
+++ src/main/java/com/honcari/controller/ReturnPageController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author yamaseki
+ *
+ */
 @Controller
 @RequestMapping("")
 public class ReturnPageController {
