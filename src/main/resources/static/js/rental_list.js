795f4698204dd5b3e9f8894ddc497a8cb7f7a6ca
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-05-18 14:17:57 +0900

    jsファイルのエラーを解消

--- src/main/resources/static/js/rental_list.js
+++ src/main/resources/static/js/rental_list.js
@@ -6,22 +6,22 @@
 		$('#flash').alert('close');
 	}, 3000);
 
-	// 延長申請時に延長後の貸出期限のデフォルト値を１週間後にセットする
-	if(document.getElementById('rental-period') != null){
-		var deadline = document.getElementById('rental-period').textContent.split("-");
-		var deadlineDate = new Date(deadline[0], deadline[1]-1, deadline[2]);
-		deadlineDate.setDate(deadlineDate.getDate()+7);
-		var yyyy = deadlineDate.getFullYear();
-		var mm = ("0"+(deadlineDate.getMonth()+1)).slice(-2);
-		var dd = ("0"+deadlineDate.getDate()).slice(-2);
-		document.getElementById("extend-date").value=yyyy+'-'+mm+'-'+dd;
-	}
-    
-	// 延長申請時の入力値チェックでエラーが出た場合はモーダルを表示
-	if (document.getElementById("error-message") !== null) {
-		$("#extend-request-form").modal();
-	}
-	
+//	延長機能を削除したためコメントアウトby湯口
+//	// 延長申請時に延長後の貸出期限のデフォルト値を１週間後にセットする
+//	if(document.getElementById('rental-period') != null){
+//		var deadline = document.getElementById('rental-period').textContent.split("-");
+//		var deadlineDate = new Date(deadline[0], deadline[1]-1, deadline[2]);
+//		deadlineDate.setDate(deadlineDate.getDate()+7);
+//		var yyyy = deadlineDate.getFullYear();
+//		var mm = ("0"+(deadlineDate.getMonth()+1)).slice(-2);
+//		var dd = ("0"+deadlineDate.getDate()).slice(-2);
+//		document.getElementById("extend-date").value=yyyy+'-'+mm+'-'+dd;
+//	}
+//    
+//	// 延長申請時の入力値チェックでエラーが出た場合はモーダルを表示
+//	if (document.getElementById("error-message") !== null) {
+//		$("#extend-request-form").modal();
+//	}
 	
 	$('.cancel-btn').click(function() {
 		if (!confirm($(this).text() + "してよろしいでしょうか?")) {
