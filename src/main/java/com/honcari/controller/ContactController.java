dce4a8cb87c1bc64827f965fd92dabb092c3429a
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-05-20 12:05:08 +0900

    お問い合わせのコントローラーを作成しました。

---
+++ src/main/java/com/honcari/controller/ContactController.java
@@ -0,0 +1,66 @@
+package com.honcari.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.honcari.domain.LoginUser;
+import com.honcari.form.ContactForm;
+import com.honcari.service.SendContactMailService;
+
+/**
+ * お問い合わせ用のコントローラー.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
+@Controller
+public class ContactController {
+	
+	@Autowired
+	private SendContactMailService sendContactMailService;
+	
+	@ModelAttribute
+	public ContactForm setUpContactForm() {
+		return new ContactForm();
+	}
+	
+	/**
+	 * お問い合わせフォームへ遷移するメソッド.
+	 * 
+	 * @param model リクエストスコープ
+	 * @param loginUser ログイン中のユーザー
+	 * @return お問い合わせ画面
+	 */
+	@RequestMapping("/show_contact")
+	public String showContact(Model model, @AuthenticationPrincipal LoginUser loginUser) {
+		model.addAttribute("user", loginUser.getUser());
+		return "contact";
+	}
+	
+	/**
+	 * お問い合わせを送信するメソッド.
+	 * 
+	 * @param contactForm お問い合わせ用フォーム
+	 * @param result エラー格納オブジェクト
+	 * @param model リクエストスコープ
+	 * @param loginUser ログイン中のユーザー
+	 * @return お問い合わせ画面
+	 */
+	@RequestMapping("/contact")
+	public String contact(@Validated ContactForm contactForm, BindingResult result, 
+			Model model, @AuthenticationPrincipal LoginUser loginUser) {
+		if(result.hasErrors()) {
+			return showContact(model, loginUser);
+		}
+		sendContactMailService.sendContactMailToCustomer(contactForm);
+		model.addAttribute("successMessage", "お問い合わせを送信しました！");
+		return "redirect:/show_contact";
+	}
+
+}
