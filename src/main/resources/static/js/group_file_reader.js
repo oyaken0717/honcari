0a112a0eaa35ec6b897284e837b84a803e8bc7aa
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-05-25 22:23:58 +0900

    画像処理用jsをグループ画像用に複製しました。

---
+++ src/main/resources/static/js/group_file_reader.js
@@ -0,0 +1,56 @@
+$(function(){
+	var name1 = $("#name1").val();
+	var name2 = $("#name2").val();
+	
+	$('.custom-file-input').on('change', handleFileSelect);
+	function handleFileSelect(evt) {
+		var sizeLimit = 1024 * 1024 * 1;
+		var file = evt.target.files[0];
+		
+		//1MBを超えているかチェック
+		if(file.size > sizeLimit) {
+			alert('1MBより大きいファイルはアップロードできません');
+			evt.target.value = ''; // inputの中身をリセット
+			return;
+		}
+			
+		var reader = new FileReader();
+		reader.onload = (function(theFile) {
+			return function(e) {
+				if (theFile.type.match('image.*')) {
+					$("#current-image").removeClass("d-block").addClass("d-none");
+					$("#input-image").remove();
+					var $html = ['<img id="input-image" class="rounded-lg mx-auto d-block border border-secondary" src="', e.target.result,'" title="', escape(theFile.name), '" width="320px" height="180px">'].join('');// 画像では画像のプレビューの表示
+					$('#image-panel').append($html);
+				} else {
+					alert('画像でないファイルはアップロードできません');
+					evt.target.value = ''; // inputの中身をリセット
+					return;
+				}
+			};
+		})(file);
+		
+		reader.readAsDataURL(file);
+		
+		$(this).next('.custom-file-label').html(escape(file.name));
+	}
+	
+	//ファイルの取消
+	$('.reset').click(function(){
+		$(this).parent().prev().children('.custom-file-label').html('ファイル選択...');
+		$("#current-image").removeClass("d-none").addClass("d-block");
+		$('#input-image').remove();
+		$('.custom-file-input').val('');
+	});
+	
+	$('#delete-image').click(function(){
+		if(confirm("現在の画像を削除してよろしいですか？") {
+			$('#current-image').removeClass("d-none").addClass("d-block").attr('src','https://'+name2+'.s3-ap-northeast-1.amazonaws.com/'+name1+'/group_default.jpg');
+			$('#image-path').val('https://'+name2+'.s3-ap-northeast-1.amazonaws.com/'+name1+'/group_default.jpg');
+			$('.custom-file-label').html('ファイル選択...');
+			$('#input-image').remove();
+			$('.custom-file-input').val('');
+		}
+	});
+	
+});
