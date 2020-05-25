1f80b4041d441d1c44e7161ad968bfa9aa94a568
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-05-25 22:14:12 +0900

    画像のプレビュー等の表示方法、処理方法を変更しました。

--- src/main/resources/static/js/file_reader.js
+++ src/main/resources/static/js/file_reader.js
@@ -1,33 +1,56 @@
-$('.custom-file-input').on('change', handleFileSelect);
-function handleFileSelect(evt) {
-        $(this).parents('.input-group').after('<div id="preview"></div>');
-
-    var files = evt.target.files;
-
-    for (var i = 0, f; f = files[i]; i++) {
-
-        var reader = new FileReader();
-
-        reader.onload = (function(theFile) {
-            return function(e) {
-                if (theFile.type.match('image.*')) {
-                    var $html = ['<div class="d-inline-block mr-1 mt-1"><img class="img-thumbnail" src="', e.target.result,'" title="', escape(theFile.name), '" style="height:100px;" /></div>'].join('');// 画像では画像のプレビューの表示
-                } else {
-                    var $html = ['<div class="d-inline-block mr-1"><span class="small">', escape(theFile.name),'</span></div>'].join('');//画像以外はファイル名のみの表示
-                }
-
-                $('#preview').append($html);
-            };
-        })(f);
-
-        reader.readAsDataURL(f);
-    }
-    $(this).next('.custom-file-label').html(+ files.length + '個のファイルを選択しました');
-}
-
-//ファイルの取消
-$('.reset').click(function(){
-    $(this).parent().prev().children('.custom-file-label').html('ファイル選択...');
-    $('#preview').remove();
-    $('.custom-file-input').val('');
-})
+$(function(){
+	var name1 = $("#name1").val();
+	var name2 = $("#name2").val();
+	
+	$('.custom-file-input').on('change', handleFileSelect);
+	function handleFileSelect(evt) {
+		var sizeLimit = 1024 * 1024 * 1;
+		var file = evt.target.files[0];
+		
+		//1MBを超えているかチェック
+		if(file.size > sizeLimit) {
+			alert('1MBより大きいファイルはアップロードできません');
+			evt.target.value = ''; // inputの中身をリセット
+			return;
+		}
+			
+		var reader = new FileReader();
+		reader.onload = (function(theFile) {
+			return function(e) {
+				if (theFile.type.match('image.*')) {
+					$("#current-image").removeClass("d-block").addClass("d-none");
+					$("#input-image").remove();
+					var $html = ['<img id="input-image" class="rounded-lg mx-auto d-block border border-secondary user-image" src="', e.target.result,'" title="', escape(theFile.name), '" width="100px" height="100px">'].join('');// 画像では画像のプレビューの表示
+					$('#image-panel').append($html);
+				} else {
+					alert('画像でないファイルはアップロードできません');
+					evt.target.value = ''; // inputの中身をリセット
+					return;
+				}
+			};
+		})(file);
+		
+		reader.readAsDataURL(file);
+		
+		$(this).next('.custom-file-label').html(escape(file.name));
+	}
+	
+	//ファイルの取消
+	$('.reset').click(function(){
+		$(this).parent().prev().children('.custom-file-label').html('ファイル選択...');
+		$("#current-image").removeClass("d-none").addClass("d-block");
+		$('#input-image').remove();
+		$('.custom-file-input').val('');
+	});
+	
+	$('#delete-image').click(function(){
+		if(confirm("現在の画像を削除してよろしいですか？")) {
+			$('#current-image').removeClass("d-none").addClass("d-block").attr('src','https://'+name2+'.s3-ap-northeast-1.amazonaws.com/'+name1+'/user_default.png');
+			$('#image-path').val('https://'+name2+'.s3-ap-northeast-1.amazonaws.com/'+name1+'/user_default.png');
+			$('.custom-file-label').html('ファイル選択...');
+			$('#input-image').remove();
+			$('.custom-file-input').val('');
+		}
+	});
+	
+});
